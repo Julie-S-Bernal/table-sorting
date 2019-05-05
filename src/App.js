1ce808f51a8b075cbf6426d878cb71df7969afe0
“Julie-S-Bernal“ <Julies.bernal@gmail.coml>
2019-05-05 13:08:49 +0100

    added my dataSet files and checked that it is rendering properly in my table

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>still working?</div>
       </header>
       <Router>
         <Route
--- src/Table.js
+++ src/Table.js
@@ -1,12 +1,34 @@
 import React, { Component} from 'react';
+import ReactTable from 'react-table';
+import "react-table/react-table.css";
+import dataSet from './tallestBuildingCities.json';
 
 
 class Table extends Component {
 
+    // 1 add create-react-table dependency
+    // 2 check that the data is working
+    // 3 sort columns
+    // check for errors
 
 render () {
+
+console.log('dataSet', dataSet);
   return(
-    <div> Working </div>
+    <div>
+      <div> List of tallest Buildings per cities</div>
+      <ReactTable
+      data={dataSet}
+      columns={[
+          {
+            Header: 'all Structures',
+            accessor : 'All\nStructures',
+          }
+      ]}
+      defaultPageSize ={40}
+      />
+
+    </div>
   )
  }
 }
